2a4dfddc71450e7c34db3ecb9ed4b3b647cb3285
Lucio Fondón <lucio.fondonterron@plexus.es>
2025-06-14 02:37:26 +0200

    fix recover

--- backend/src/database/data-source.ts
+++ backend/src/database/data-source.ts
@@ -1,18 +1,20 @@
 import { DataSource } from 'typeorm';
 import { config } from 'dotenv';
-import { join } from 'path';
+import { Bus } from '../entities/bus.entity';
+import { Pupitre } from '../entities/pupitre.entity';
+import { Validator } from '../entities/validator.entity';
+import { Camera } from '../entities/camera.entity';
 
 config();
 
 export default new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT || '5432'),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  entities: [join(__dirname, '..', 'entities', '*.entity{.ts,.js}')],
-  migrations: [join(__dirname, 'migrations', '*.ts')],
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  database: process.env.DB_DATABASE || 'devices_monitoring',
+  entities: [Bus, Pupitre, Validator, Camera],
+  migrations: ['src/database/migrations/*.ts'],
   synchronize: false,
-  logging: process.env.NODE_ENV !== 'production',
 }); 
